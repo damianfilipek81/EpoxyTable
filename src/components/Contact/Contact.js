f02bd21b381a9bf617e630648c9b15fefecb08b5
Damian <damianfilipek81@gmail.com>
2021-05-13 08:45:13 +0200

    Add styles to Contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import styles from './Contact.module.scss';
+import { Container } from '@material-ui/core';
 
 const Contact = () => (
   <div className={styles.root}>
+    <Container className={styles.container}>
+      <h2 className={styles.title}>Kontakt</h2>
+      <div className={styles.contactWrapper}>
+        <div className={styles.leftInfo}>
 
+        </div>
+        <div className={styles.rightInfo}>
+
+        </div>
+      </div>
+    </Container>
   </div>
 )
 
